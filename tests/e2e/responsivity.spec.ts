4ad7e154c6d3b153796642f72da64e811dcb055b
RubTheKid <jadercardoso@tuta.io>
2025-10-06 16:27:18 -0300

    fix: solving lint error

--- tests/e2e/responsivity.spec.ts
+++ tests/e2e/responsivity.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test'
+import { test } from '@playwright/test'
 import { clickFirstFavoriteButton, gotoHome, mobileLayout, verifyFavoritesCount, verifyFirstFavoriteButton, verifyFirstPokemonCard, verifyLoading, verifyPokemonGrid, verifyTitleSubtitle } from './support/pokemon.helpers';
 
 test.describe('design resposivity', () => {
