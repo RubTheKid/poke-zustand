529f96bc9d4fc9224ae35076b442a6b4241ac61b
RubTheKid <jadercardoso@tuta.io>
2025-10-06 16:21:57 -0300

    test: refactoring tests

--- tests/e2e/responsivity.spec.ts
+++ tests/e2e/responsivity.spec.ts
@@ -1,27 +1,18 @@
 import { test, expect } from '@playwright/test'
-import { clickFirstFavoriteButton, gotoHome, mobileLayout, verifyFavoritesCount, verifyFirstFavoriteButton, verifyFirstPokemonCard, verifyPokemonGrid, verifyTitleAndSubtitle } from './support/pokemon.helpers';
+import { clickFirstFavoriteButton, gotoHome, mobileLayout, verifyFavoritesCount, verifyFirstFavoriteButton, verifyFirstPokemonCard, verifyLoading, verifyPokemonGrid, verifyTitleSubtitle } from './support/pokemon.helpers';
 
 test.describe('design resposivity', () => {
     test('should have mobile layout', async ({ page }) => {
         await mobileLayout(page);
         await gotoHome(page);
-
-        await expect(page.getByText('Loading Pokémon...')).toBeHidden();
-
-        await verifyTitleAndSubtitle(page)
-
+        await verifyLoading(page)
+        await verifyTitleSubtitle(page)
         await verifyFirstPokemonCard(page)
-
-
         await verifyPokemonGrid(page)
-
-
         await verifyFirstFavoriteButton(page)
         await clickFirstFavoriteButton(page)
-
         await verifyFavoritesCount(page)
         await clickFirstFavoriteButton(page)
-
         await verifyFavoritesCount(page)
     })
 })
